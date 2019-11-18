fbe277f749267f227cee11bbe89e44007b7938b1
iossocket <avx302@gmail.com>
2019-11-18 20:52:34 +0800

    add localhost 3000 origin in cors

--- foodie-api/src/main/java/com/iossocket/config/CorsConfig.java
+++ foodie-api/src/main/java/com/iossocket/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.iossocket.config;
 
-import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,11 +7,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-@NoArgsConstructor
 public class CorsConfig {
+    public CorsConfig() { }
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("*");
 
         config.setAllowCredentials(true);
