3b2d49b845e77df1e1593ea5b957a488768c4bba
iossocket <avx302@gmail.com>
2020-01-03 21:44:15 +0800

    add resource mapping in config class

--- foodie-api/src/main/java/com/iossocket/config/WebMvcConfig.java
+++ foodie-api/src/main/java/com/iossocket/config/WebMvcConfig.java
@@ -4,9 +4,20 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:/Users/zhuxueliang/Public/workspace/")
+                .addResourceLocations("classpath:/META-INFO/resources/"); // for swagger 2
+        // add resource mapping
+        // http://localhost:8088/avatar/1908017YR51G1XWH/avatar-1908017YR51G1XWH.png
+    }
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
